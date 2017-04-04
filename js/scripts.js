d766bdf155755132f36c3dd4de03be2334a31ecd
Anatoliy Klots and Chitroopa Manikkavasagam <chitroopa@>
2017-04-04 09:09:25 -0700

    add empty fields removal

--- js/scripts.js
+++ js/scripts.js
@@ -51,8 +51,15 @@ $(document).ready(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
-      newContact.address.push(newAddress);
+      if(!inputtedStreet || !inputtedCity || !inputtedState)
+      {
+        $(this).remove(".new-address");
+      }
+      else
+      {
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+        newContact.address.push(newAddress);
+      }
     });
 
 
